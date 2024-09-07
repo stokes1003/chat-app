41627dc521f10799055dad15246e98f7420f8a01
Sam Stokes <stokes1003@gmail.com>
2024-09-06 20:12:11 -0500

    layout edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
 import '@mantine/core/styles.css';
 import React from 'react';
 import Header from '@/components/Header';
